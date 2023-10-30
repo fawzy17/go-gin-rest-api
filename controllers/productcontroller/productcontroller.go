3640c23a1a653193e466a6b489c7aff26cc59f41
jidokaray <ray@jidokasystem.co.id>
2023-10-31 01:00:39 +0700

    create update delete

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -10,6 +10,7 @@ import (
 )
 
 func Index(c *gin.Context) {
+
 	var products []models.Product
 
 	config.DB.Find(&products)
@@ -37,7 +38,7 @@ func Show(c *gin.Context) {
 			})
 			return
 		default:
-			c.AbortWithStatusJSON(http.StatusNotFound, models.Response{
+			c.AbortWithStatusJSON(http.StatusInternalServerError, models.Response{
 				StatusCode:    http.StatusInternalServerError,
 				StatusMessage: http.StatusText(http.StatusInternalServerError),
 				Status:        "failed",
@@ -56,11 +57,78 @@ func Show(c *gin.Context) {
 }
 
 func Create(c *gin.Context) {
+	var product models.Product
 
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Response{
+			StatusCode:    http.StatusInternalServerError,
+			StatusMessage: http.StatusText(http.StatusBadRequest),
+			Status:        "failed",
+			Data:          err.Error(),
+		})
+		return
+	}
+
+	config.DB.Create(&product)
+	c.JSON(http.StatusCreated, models.Response{
+		StatusCode:    http.StatusCreated,
+		StatusMessage: http.StatusText(http.StatusCreated),
+		Status:        "success",
+		Data:          product,
+	})
 }
 func Update(c *gin.Context) {
+	var product models.Product
 
+	id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Response{
+			StatusCode:    http.StatusBadRequest,
+			StatusMessage: http.StatusText(http.StatusBadRequest),
+			Status:        "failed",
+			Data:          err.Error(),
+		})
+		return
+	}
+
+	if config.DB.Model(&product).Where("id = ?", id).Updates(&product).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Response{
+			StatusCode:    http.StatusBadRequest,
+			StatusMessage: http.StatusText(http.StatusBadRequest),
+			Status:        "failed",
+			Data:          "Update data gagal",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		StatusCode:    http.StatusOK,
+		StatusMessage: http.StatusText(http.StatusOK),
+		Status:        "success",
+		Data:          &product,
+	})
 }
 func Delete(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
 
+	if config.DB.Delete(&product, id).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusNotFound, models.Response{
+			StatusCode:    http.StatusNotFound,
+			StatusMessage: http.StatusText(http.StatusNotFound),
+			Status:        "failed",
+			Data: map[string]interface{}{
+				"message": "id tidak ditemukan",
+			},
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		StatusCode:    http.StatusOK,
+		StatusMessage: http.StatusText(http.StatusOK),
+		Status:        "success",
+		Data:          product,
+	})
 }
